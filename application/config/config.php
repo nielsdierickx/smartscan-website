e7fd6faa480cdc9b408273911cdc0432133491ba
Niels Dierickx <nielsdierickx@gmail.com>
2012-02-02 18:23:31 +0100

    config update

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://localhost/smartscan-website2/';
+$config['base_url']	= 'http://localhost/smartscan-website/';
 
 /*
 |--------------------------------------------------------------------------
